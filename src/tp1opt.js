01ca0747f3f89920ff07815121cab4537a2c9892
Manuel Kucharuk <manuelkucharuk@gmail.com>
2022-03-02 20:14:06 -0300

    Agregado Eventlistener

--- src/tp1opt.js
+++ src/tp1opt.js
@@ -1,10 +1,21 @@
 "use strict"
 
+window.onload = ()=>{
+    document.getElementById("tipoSeguro").addEventListener("change",mostrarValor)
+}
+
+
 function mostrarValor(){
-    var valor = 0
-    var opcion = document.getElementById("tipoSeguro").value
-    if (opcion=="basico") valor = 500
-    else if (opcion=="intermedio") valor = 1000
-    else if (opcion=="premium") valor = 1500
+    let valor = 0
+    let opcion = document.getElementById("tipoSeguro").value
+
+    if (opcion=="basico") 
+        valor = 500
+    else if (opcion=="intermedio") 
+        valor = 1000
+    else if (opcion=="premium")
+        valor = 1500
+    else valor = ""    
+
     document.getElementById("precioSeguro").innerHTML = "$"+valor.toString()
 }
